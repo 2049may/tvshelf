8ec152170d8f5b71ecb74db9b2aa7c0e9ee08654
Mayssa Lakhal <mayssa.lakhal@etu.univ-lyon1.fr>
2025-03-20 15:44:56 +0100

    init() : scaffold seriescreen

--- app/src/main/java/com/pirrera/tvshelf/components/SerieScreen.kt
+++ app/src/main/java/com/pirrera/tvshelf/components/SerieScreen.kt
@@ -1,12 +1,20 @@
 package com.pirrera.tvshelf.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.pirrera.tvshelf.data.Series
 import com.pirrera.tvshelf.destinations.HomeScreenDestination
+import com.pirrera.tvshelf.destinations.MainScreenDestination
 import com.pirrera.tvshelf.destinations.SerieScreenDestination
+import com.pirrera.tvshelf.ui.theme.Background
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.result.ResultRecipient
@@ -15,9 +23,28 @@ import com.ramcosta.composedestinations.result.ResultRecipient
 @Destination
 @Composable
 fun SerieScreen(navigator: DestinationsNavigator, serieName : String,serieOverview : String){
-    Column {
-        Text(serieName)
-        Text(serieOverview)
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize()
+            .background(Background),
+        topBar = {
+            Button(onClick = { navigator.navigate(MainScreenDestination) }) {
+                Text("Back")
+            }
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text(serieName)
+            Text(serieOverview)
+        }
     }
+
+//    Button(onClick = { navigator.navigate(MainScreenDestination) }) {
+//        Text("Back")
+//    }
+//    Column {
+//        Text(serieName)
+//        Text(serieOverview)
+//    }
 
 }
